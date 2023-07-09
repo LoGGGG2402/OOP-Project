3bcd01ddcf05be4cd14a5a7167403da5ff4852d7
LOGGGG <long.pph214964@sis.hust.edu.vn>
2023-07-10 00:38:54 +0700

    DynastyList aaA

--- src/main/java/merge/MonumentList.java
+++ src/main/java/merge/MonumentList.java
@@ -15,7 +15,7 @@ public class MonumentList extends EntityList{
     @Override
     protected void merge() {
         for (Entity entity: getBaseEntities()) {
-            if (entity.getName() == null) continue;
+            if (entity.getName() == null || entity.getName().isEmpty()) continue;
             if(nameList != null && nameList.contains(entity.getName())){
                 Entity entity1 = getEntities().get(nameList.indexOf(entity.getName()));
                 if (entity1 == null){
